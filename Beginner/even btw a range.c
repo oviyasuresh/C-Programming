faa704e92415f6689d37dfae1f6869ecfa7c13cd
oviyasuresh <oviyasuresh09@gmail.com>
2017-12-20 19:49:31 +0530

    Create even btw a range.c

---
+++ Beginner/even btw a range.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(void) 
+
+{
+
+int i;
+
+printf("\n the even numbers are:");
+
+for(i=10;i<=20;i++)
+
+{
+
+if(i%2==0)
+
+printf("%d",i);
+
+printf("\n");
+
+}
+
+ 
+
+return 0;
+
+}
