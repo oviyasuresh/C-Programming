be8a61650d21a71150a233c0ac1495dae0438b84
oviyasuresh <oviyasuresh09@gmail.com>
2017-12-26 19:28:27 +0530

    Create count the spaces in a line.c

---
+++ Beginner/count the spaces in a line.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	char str[50];
+	int i,count=0;
+	printf("enter the sentence:");
+	scanf("%[^\n]s",str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]==' ')
+		{
+			count=count+1;
+		}
+	}
+	printf("\nno of white spaces=%d",count);
+	return 0;
+}
