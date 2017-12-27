ffd66d7a1c9a689c14c2c8b3bc1a0361d337a347
oviyasuresh <oviyasuresh09@gmail.com>
2017-12-27 19:18:42 +0530

    Create check if the num is within the range.c

---
+++ Beginner/check if the num is within the range.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n;
+	printf("enter the number:");
+	scanf("%d",&n);
+	if(n>=1 && n<=10)
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+	return 0;
+}
