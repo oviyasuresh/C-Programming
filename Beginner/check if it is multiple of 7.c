85d26952ae2c54253f71a29b822eb67fe646a2df
oviyasuresh <oviyasuresh09@gmail.com>
2017-12-29 20:03:26 +0530

    Create check if it is multiple of 7.c

---
+++ Beginner/check if it is multiple of 7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n;
+	printf("enter the number:");
+	scanf("%d",&n);
+	if(n%7==0)
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+	return 0;
+}
