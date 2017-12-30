2c1c683cb8c4d46f8d1b2b857ae601a666b2e94a
oviyasuresh <oviyasuresh09@gmail.com>
2017-12-30 20:30:09 +0530

    Create check if it is multiple of 13.c

---
+++ Beginner/check if it is multiple of 13.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+int main(void) 
+
+{
+
+	int n;
+
+	printf("enter the number:");
+
+	scanf("%d",&n);
+
+	if(n%13==0)
+
+	{
+
+		printf("\n yes");
+
+	}
+
+	else
+
+	{
+
+		printf("\n no");
+
+	}
+
+	return 0;
+
+}
