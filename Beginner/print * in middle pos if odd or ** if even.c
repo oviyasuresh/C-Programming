f760691d6abb684d493a224cb21ac034caab2a60
oviyasuresh <oviyasuresh09@gmail.com>
2017-12-30 20:09:20 +0530

    Create print * in middle pos if odd or ** if even.c

---
+++ Beginner/print * in middle pos if odd or ** if even.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+	char str[30];
+	int n,i;
+	printf("enter the string:");
+	scanf("%s",str);
+	n=strlen(str);
+	if(n%2==0)
+	{
+		str[n/2]='*';
+		str[(n/2)-1]='*';
+	}
+	else
+	{
+		str[n/2]='*';
+	}
+	printf("\n%s",str);
+	return 0;
+}
