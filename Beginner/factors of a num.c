afc2899872ac34e5b87f95ba78b7483af263dbc2
oviyasuresh <oviyasuresh09@gmail.com>
2017-12-30 20:22:35 +0530

    Create factors of a num.c

---
+++ Beginner/factors of a num.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n,i;
+	printf("enter the number:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			printf("%d ",i);
+		}
+	}
+	return 0;
+}
