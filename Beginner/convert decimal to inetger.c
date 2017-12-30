fc574493645d1a4a5c0076f0d54afb42005286e1
oviyasuresh <oviyasuresh09@gmail.com>
2017-12-30 19:58:22 +0530

    Create convert decimal to inetger.c

---
+++ Beginner/convert decimal to inetger.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+int main(void) 
+{
+	float num;
+	int x;
+	printf("\n enter the num:");
+	scanf("%f",&num);
+	x=(int)(num+0.5);
+	printf("\n%d",x);
+	return 0;
+}
