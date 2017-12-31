bed415cef1881ec312da48897b7539d7b4f3612e
oviyasuresh <oviyasuresh09@gmail.com>
2017-12-31 20:26:02 +0530

    Create gcd.c

---
+++ Beginner/gcd.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) 
+{
+int x,y,remainder,gcd;
+printf("\n enter the first number:");
+scanf("%d",&x);
+printf("\n enter the second number:");
+scanf("%d",&y);
+do
+{
+remainder=x%y;
+if(remainder==0)
+break;
+x=y;
+y=remainder;
+}while(remainder!=0);
+gcd=y;
+printf("\n the gcd of the given number is: %d",gcd);
+return 0;
+}
