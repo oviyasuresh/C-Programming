03efcd766eaf6a378484e655c25b1e3540040dd5
oviyasuresh <oviyasuresh09@gmail.com>
2017-12-31 20:36:12 +0530

    Create prints num in alphanumeric str.c

---
+++ Beginner/prints num in alphanumeric str.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+	char str[50];
+	int i,n;
+	printf("\nenter the alphanumeric string:\n");
+	scanf("%s",str);
+	n=strlen(str);
+	for(i=0;i<n;i++)
+	{
+		if(str[i]>='0' && str[i]<='9')
+		{
+			printf("%c",str[i]);
+		}
+	}
+	return 0;
+}
