8595780eb3deae8dc9f5e3cb59a69e3c89442120
oviyasuresh <oviyasuresh09@gmail.com>
2018-01-08 20:48:12 +0530

    Create prime btw range including range.c

---
+++ Player/prime btw range including range.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+
+
+int main(void) 
+
+{
+
+	int l,r,i,j,count=0,flag=1;
+
+	scanf("%d",&l);
+
+	scanf(" %d",&r);
+
+	if(l==2)
+
+	{
+
+		count++;
+
+	}
+
+	for(i=l;i<=r;i++)
+
+	{
+
+		for(j=2;j<i;j++)
+
+		{
+
+			if(i%j==0)
+
+			{
+
+				flag=0;
+
+				break;
+
+				
+
+			}
+
+			
+
+		}
+
+			if(flag==1)
+
+			{
+
+				count=count+1;
+
+			}
+
+		
+
+		
+
+	}
+
+
+
+	printf("\n%d",count);
+
+	return 0;
+
+}
