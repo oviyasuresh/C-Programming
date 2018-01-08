e8b86789c015a36206ce3e1c8af9fb52e713d6f2
oviyasuresh <oviyasuresh09@gmail.com>
2018-01-08 18:31:18 +0530

    Create reverse of a num.c

---
+++ repeated/reverse of a num.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main(void) 
+
+ 
+
+{
+
+int n,remainder,reverse=0;
+
+ 
+
+int result;
+
+ 
+
+printf("\n enter the number:");
+
+ 
+
+scanf("%d",&n);
+
+ 
+
+while(n!=0)
+
+ 
+
+{
+
+ 
+
+remainder=n%10;
+
+ 
+
+reverse=reverse*10+remainder;
+
+ 
+
+n=n/10;
+
+ 
+
+}
+
+ 
+
+printf("\n%d",reverse);
+
+return 0;
+
+ 
+
+}
