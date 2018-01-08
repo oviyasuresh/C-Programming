fb495806c5d22804db6b97af9a9a38710e6fb4c3
oviyasuresh <oviyasuresh09@gmail.com>
2018-01-08 19:20:33 +0530

    Create swap odd even places.c

---
+++ Beginner/swap odd even places.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+#include<string.h>
+
+int main(void)
+
+{
+
+	char string[10];
+
+	int i,temp,n;
+
+	printf("enter the string:");
+
+	gets(string);
+
+	n=strlen(string);
+
+	if(n%2==0)
+
+	{
+
+		for(i=0;i<n;i+=2)
+
+		{
+
+			temp=string[i];
+
+			string[i]=string[i+1];
+
+			string[i+1]=temp;
+
+		}
+
+		puts(string);
+
+	}
+
+	else
+
+	{
+
+		for(i=0;i<n-1;i+=2)
+
+		{
+
+			temp=string[i];
+
+			string[i]=string[i+1];
+
+			string[i+1]=temp;
+
+		}
+
+		puts(string);
+
+		
+
+	}
+
+
+
+
+
+return 0;
+
+}
