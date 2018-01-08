3b888ddd79f3f8f15e9a5175d1993e9f4dac543a
oviyasuresh <oviyasuresh09@gmail.com>
2018-01-08 20:50:52 +0530

    Create factorial.c

---
+++ Player/factorial.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+int main(void)
+
+{
+
+	int i,num,fact=1;
+
+	printf("enter the number:");
+
+	scanf("%d",&num);
+
+	for(i=1;i<=num;i++)
+
+	{
+
+		fact=fact*i;
+
+	}
+
+	printf("\n%d",fact);
+
+	return 0;
+
+}
