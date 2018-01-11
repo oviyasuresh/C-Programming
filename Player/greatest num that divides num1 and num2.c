9d442cd35fc39cf2f0f619fc571c3fd3009d1b3a
oviyasuresh <oviyasuresh09@gmail.com>
2018-01-11 19:48:55 +0530

    Create greatest num that divides num1 and num2.c

---
+++ Player/greatest num that divides num1 and num2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n,k,i,len;
+	scanf("%d %d",&n,&k);
+	if(n<k)
+	{
+		len=n;
+	}
+	else
+	{
+		len=k;
+	}
+	for(i=len;i>=2;i--)
+	{
+		if(n%i==0 && k%i==0)
+		{
+			printf("%d",i);		
+		}
+	}
+
+	return 0;
+}
