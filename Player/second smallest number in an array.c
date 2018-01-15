884c080b49eee6264ed5ae246c4e1768b61f97fa
oviyasuresh <oviyasuresh09@gmail.com>
2018-01-15 16:38:14 +0530

    Create second smallest number in an array.c

---
+++ Player/second smallest number in an array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n,array[50],i,j,temp;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d ",&array[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+		if(array[i]>array[j])
+		{
+			temp=array[i];
+			array[i]=array[j];
+			array[j]=temp;
+		}
+		}
+	}
+	printf("\n%d",array[1]);
+		
+	
+	return 0;
+}
