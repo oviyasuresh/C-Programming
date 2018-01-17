fc5257b30cb9a51fa3a32897784dd87b4480d800
oviyasuresh <oviyasuresh09@gmail.com>
2018-01-17 19:17:41 +0530

    Create min int the num is divided where result is odd.c

---
+++ Player/min int the num is divided where result is odd.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n,i,x;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			x=n/i;
+			if(x%2==1)
+			{
+				printf("%d",i);
+				break;
+			}
+		}
+		
+	}
+	return 0;
+}
