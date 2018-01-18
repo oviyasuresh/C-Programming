07fb2f0ce1774e2508bb6baaddef1ba7a376a39b
oviyasuresh <oviyasuresh09@gmail.com>
2018-01-18 19:03:01 +0530

    Update print nums that is repeated k times in array.c

--- Player/print nums that is repeated k times in array.c
+++ Player/print nums that is repeated k times in array.c
@@ -4,25 +4,27 @@ int main(void)
 {
 	int n,k;
 	scanf("%d %d",&n,&k);
-	int array[n],i,j,count=1;
+	int array[n],i,j;
 	for(i=0;i<n;i++)
 	{
 		scanf("%d ",&array[i]);
 	}
 	for(i=0;i<n;i++)
 	{
+		int count=1;
 		for(j=i+1;j<n;j++)
 		{
 			if(array[i]==array[j])
 			{
 				count=count+1;
-				if(count==k)
-				{
-					printf("%d",array[i]);
-				}
 			}
 		}
+		if(count==k)
+		{
+			printf("%d",array[i]);
+			break;
+			
+		}
 	}
-	
 	return 0;
 }
