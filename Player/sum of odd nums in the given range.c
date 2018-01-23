e3349d68a4b06085d7aaff6f426f58cf4ed0f08d
oviyasuresh <oviyasuresh09@gmail.com>
2018-01-23 21:01:28 +0530

    Create sum of odd nums in the given range.c

---
+++ Player/sum of odd nums in the given range.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void)
+{
+    int l,r,i,sum=0;
+    scanf("%d %d",&l,&r);
+    for(i=l;i<=r;i++)
+    {
+        if(i%2==1)
+        {
+            sum=sum+i;
+        }
+    }
+    printf("%d",sum);
+}
