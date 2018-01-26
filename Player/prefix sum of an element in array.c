e60802fcc47b798852fcde6440fa21d7538b0490
oviyasuresh <oviyasuresh09@gmail.com>
2018-01-26 20:27:44 +0530

    Create prefix sum of an element in array.c

---
+++ Player/prefix sum of an element in array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n;
+	scanf("%d",&n);
+	int a[n],i,sum=0;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d ",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=sum+a[i];
+		printf("%d ",sum);
+	}
+	return 0;
+}
