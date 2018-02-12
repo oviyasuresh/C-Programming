70f4d5168610f592628766d26aba8f7ed94d709b
oviyasuresh <oviyasuresh09@gmail.com>
2018-02-12 19:54:19 +0530

    Create sum of squares of the digits1.c

---
+++ repeated/sum of squares of the digits1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+ 
+
+int main(void) 
+
+{
+
+	int num,sum=0,sq,rem;
+
+	scanf("%d",&num);
+
+	while(num!=0)
+
+	{
+
+		rem=num%10;
+
+		sq=rem*rem;
+
+		sum=sum+sq;
+
+		num=num/10;
+
+	}
+
+	printf("\n%d",sum);
+
+	return 0;
+
+}
+
+ 
