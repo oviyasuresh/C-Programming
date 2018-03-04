30fa60b0542cdd7bd818f55df279d63536c5d5df
oviyasuresh <oviyasuresh09@gmail.com>
2018-03-04 19:56:04 +0530

    Create fing if the element k exists in the array.c

---
+++ Player/fing if the element k exists in the array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n,k;
+	scanf("%d %d",&n,&k);
+	int array[n],i,flag=0;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d ",&array[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(array[i]==k)
+		{
+			flag=1;
+			break;
+		}
+	}
+	if(flag==1)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
