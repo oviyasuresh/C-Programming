3d41bb4f8b27c5f0324d6b7cfb82196e06df2161
oviyasuresh <oviyasuresh09@gmail.com>
2018-03-20 19:09:14 +0530

    Create print the even numbers which are present in odd positions and odd numbers that are present in even positions.c

---
+++ Hunter/print the even numbers which are present in odd positions and odd numbers that are present in even positions.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n;
+	scanf("%d",&n);
+	int a[n],i;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d ",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(i%2==0)
+		{
+			if(a[i]%2!=0)
+			{
+				printf("%d ",a[i]);
+			}
+		}
+		if(i%2!=0)
+		{
+			if(a[i]%2==0)
+			{
+				printf("%d ",a[i]);
+			}
+		}
+	}
+	return 0;
+}
