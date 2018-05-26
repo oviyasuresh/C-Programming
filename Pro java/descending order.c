3e3f5172a0e661de7b214f2a0342d601c7d97c22
oviyasuresh <oviyasuresh09@gmail.com>
2018-05-26 21:47:43 +0530

    Create descending order.c

---
+++ Pro java/descending order.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() 
+{
+    int n,a[10],i,j,temp=0;
+	scanf("%d ",&n);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d ",&a[i]);
+	}
+	for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+        if(a[i]<a[j])
+        {
+          temp=a[i];
+          a[i]=a[j];
+          a[j]=temp;
+        }
+        }  printf("%d\n",a[i]);
+    }
+	return 0;
+}
