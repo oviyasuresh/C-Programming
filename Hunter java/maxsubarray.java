2aa7a3a1dee0adc418d3d732c131d750c158c755
oviyasuresh <oviyasuresh09@gmail.com>
2018-05-28 19:03:53 +0530

    Update maxsubarray.java

--- Hunter java/maxsubarray.java
+++ Hunter java/maxsubarray.java
@@ -6,19 +6,19 @@ public class maxsubarray
 		int n,sum=0,i,max,j,k;
 		Scanner scan=new Scanner(System.in);
 		n=scan.nextInt();
-		int a[]=new int[n];
+		int ar[]=new int[n];
 		for(i=0;i<n;i++)
 		{
-			a[i]=scan.nextInt();
+			ar[i]=scan.nextInt();
 		}
-	    max=a[0];
+	    max=ar[0];
 		for(i=0;i<n;i++)
 		{	 
 		  sum=0;
-		  sum=a[i];
+		  sum=ar[i];
 		  for(j=i+1;j<n;j++)
 		  {
-			sum=sum+a[j];
+			sum=sum+ar[j];
 			
 			if(sum>max)
 			   max=sum;
