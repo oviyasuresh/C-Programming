56e2581bb874104315efec963d528345180476c3
oviyasuresh <oviyasuresh09@gmail.com>
2018-05-29 19:59:55 +0530

    Update reverseorderofwords.java

--- Hunter java/reverseorderofwords.java
+++ Hunter java/reverseorderofwords.java
@@ -7,9 +7,9 @@ public class reverseorderofwords {
 	public static void main(String[] args)
 	{
 		Scanner sc=new Scanner(System.in);
-		String s=sc.nextLine();
-		//int n=s.length();
-		String[] str=s.split("\\s");
+		String s1=sc.nextLine();
+		//int n=s1.length();
+		String[] str=s1.split("\\s");
 		int n1=str.length;
 		for(int i=n1-1;i>=0;i--)
 		{
